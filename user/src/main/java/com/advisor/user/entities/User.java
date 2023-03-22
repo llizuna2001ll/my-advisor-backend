222907d79db3b40702f62ff64261aeeb0c0cad90
Nouhaila Zahraoui <zehraoui.1998@gmail.com>
2023-03-22 21:59:45 +0000

    user_micro_service_V1

---
+++ user/src/main/java/com/advisor/user/entities/User.java
@@ -0,0 +1,39 @@
+package com.advisor.user.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.sql.Blob;
+import java.util.Date;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Document("User")
+
+    public class User {
+    @Id
+    @Indexed(unique = true)
+    private String id;
+    @Field(name = "username")
+
+    private String username;
+    @Field(name = "email")
+
+    private String email;
+    @Field(name = "dateCreation")
+
+    private Date dateCreation;
+    @Field(name = "password")
+
+    private String password;
+
+}
---
+++ user/src/main/java/com/advisor/user/services/UserService.java
@@ -0,0 +1,47 @@
+package com.advisor.user.services;
+
+import com.advisor.user.entities.User;
+import com.advisor.user.repositories.UserRepository;
+import com.advisor.user.entities.User;
+import com.advisor.user.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void addUser(User user){
+        userRepository.insert(user);
+    }
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(String id){
+
+        return userRepository.findById(id);
+    }
+
+    public void updateUser(User user){
+        User savedUser = userRepository.findById(user.getId()).orElseThrow(() -> new RuntimeException(
+                String.format("cannot find user with id "+user.getId())
+        ));
+        savedUser.setUsername(user.getUsername());
+        savedUser.setEmail(user.getEmail());
+        savedUser.setPassword(user.getPassword());
+        savedUser.setDateCreation(user.getDateCreation());
+        userRepository.save(user);
+    }
+    public void deleteUser(String id){
+        userRepository.deleteById(id);
+    }
+
+}
